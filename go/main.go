4a9b66d5710f92b3878e999cb7a973e5804140a1
amirmosio <amir.mn.mosio@gmail.com>
2020-10-26 18:22:08 +0330

    little test stuff

---
+++ go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleSum(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		fmt.Println(r)
+		fmt.Fprintf(w, "Hello")
+	}
+
+}
+
+func main() {
+	var root = "/"
+	var apiLanguage = "go"
+	http.Handle(root, http.FileServer(http.Dir("./../front")))
+	http.HandleFunc(root+apiLanguage, handleSum)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
