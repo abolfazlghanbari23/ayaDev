30114ad90c1ff88911170f7d9bb35e7f0f5108bf
Abolfazl <abolfazlghanbarishce@gmail.com>
2020-10-27 13:29:00 +0330

    sha256 nodejs

--- nodejs/app.js
+++
@@ -1,9 +0,0 @@
-var SHA256 = require("crypto-js/sha256");
-
-function sayHello(name) {
-    console.log('Hello ' + name);
-    const hashDigest = SHA256(2 + 5);
-    console.log(hashDigest);
-}
-
-sayHello('Abolfazl');
---
+++ nodejs/write.js
@@ -0,0 +1,5 @@
+
+
+function getLine(lineNumber) {
+
+}
