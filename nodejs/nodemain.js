97c59e6700bee07c4b05e975184552da81a40f05
Abolfazl <abolfazlghanbarishce@gmail.com>
2020-10-29 08:50:37 +0330

    minor changes

--- nodejs/nodemain.js
+++ nodejs/nodemain.js
@@ -3,43 +3,45 @@ let app = express();
 const port = 8081;
 const nthline = require('nthline');
 var hash = require('hash.js');
-var cors = require('cors');
 
-
-app.use(
-    express.urlencoded({
-        extended: false
-    })
-)
 
 app.use(express.json());
-app.use(cors);
 
 
 app.post('/nodejs/sha256', (req, res) => {
-    const firstNum = req.body.firstNum;
-    const secondNum = req.body.secondNum;
-    console.log(req.body.firstNum);
-    console.log(req.body.secondNum);
-
-    // if (typeof firstNum != "number" || typeof secondNum != "number") {
-    //     response.send('Invalid Input :(');
-    // }
-
+    const firstNum = (req.body.firstNum);
+    const secondNum = (req.body.secondNum);
+    console.log(firstNum);
+    console.log(secondNum);
     const sum = firstNum + secondNum;
+
+    if (typeof firstNum != "number" || typeof secondNum != "number") {
+        res.send('Invalid Input :(');
+    }
+
     res.set('x-test', 'ghanbar').send({ sum: hash.sha256().update(sum).digest('hex') });
 });
 
-app.get('/nodejs/write', (req, res) => {
-    const lineNumber = req.body.lineNumber;
-    console.log(req.body.lineNumber);
 
+
+app.get('/nodejs/write', (req, res) => {
+    var lineNumber = req.params.lineNumber;
+    console.log(lineNumber);
+
+
+    if (typeof lineNumber != "number") {
+        lineNumber = 1;
+        // res.send('Invalid Input :(');
+    }
     if (lineNumber < 1 || lineNumber > 100) {
         res.send('Invalid Input :(');
     }
 
-    nthline(4, 'file.in').then(line => res.send(line))
+    nthline(lineNumber, 'file.in').then(line => res.send(line))
 });
+
+
+
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
